9f7546ada91e34771b2cdbc721057c378cc2a778
merlox <150959690+merloxinho@users.noreply.github.com>
2024-03-17 17:18:02 +0100

    fixed message form

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,14 +3,40 @@ import {motion} from 'framer-motion'
 import {Image} from '@nextui-org/react'
 import {Card, CardHeader, CardBody, Button, Input} from "@nextui-org/react";
 import emailjs from '@emailjs/browser';
-import {useRef} from 'react'
+import {useRef, useState} from 'react'
 
 
 const Contact = () => {
+
+        const emailRef = useRef();
+        const messageRef = useRef();
+        const buttonRef = useRef();
         const form = useRef();
+
+        const [messageSent, setMessageSent] = useState(false);
+        const [emailValue, setEmailValue] = useState("");
+        const [messageValue, setMessageValue] = useState("");
+
+        const handleEmailChange = (e) => {
+          setEmailValue(e.target.value);
+        }
+        const handleMessageChange = (e) => {
+          setMessageValue(e.target.value);
+        }
+
+        const handleButton = () => {
+          setTimeout(() => {
+            setMessageSent(false);
+            setMessageValue('');
+            setEmailValue('');
+            
+          }, 3000);
+        }
       
         const sendEmail = (e) => {
           e.preventDefault();
+
+          if (emailValue !== '' && messageValue !== ''){
       
           emailjs
             .sendForm('service_kplsa7a', 'template_vupegj4', form.current, {
@@ -19,15 +45,16 @@ const Contact = () => {
             .then(
               () => {
                 console.log('SUCCESS!');
-                document.getElementById("1").textContent = "";
-                document.getElementById("2").textContent = "";
-                document.getElementById("3").textContent = "";
+                setMessageSent(true);
               },
               (error) => {
                 console.log('FAILED...', error.text);
               },
             );
-        };
+          } else {
+            alert(`Email or message can't be empty`);
+          }
+    }
   return (
     <section className='flex w-full p-12 md:p-0 md:h-screen bg-gray-100 select-none'>
     <div
@@ -36,11 +63,15 @@ const Contact = () => {
             <motion.h1 viewport={{once:true}} initial={{scale:0}} whileInView={{scale:1}} transition={{delay:0.5}} className='font-bold text-4xl md:text-6xl'>Contact Me 📧</motion.h1>
         </div>
         <motion.div viewport={{once:true}} initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay:0.5}} className='mx-auto flex flex-col mx-auto justify-center gap-8 w-5/6 select-none z-10 '>
-            <form ref={form} onSubmit={sendEmail} className='mx-auto flex flex-col justify-center gap-8 w-full'>     
+            <form ref={form} id='form1' onSubmit={(e) => sendEmail(e)} className='mx-auto flex flex-col justify-center gap-8 w-full'>     
             <Input
             variant='bordered'
             label="Email"
+            value={emailValue}
+            onChange={handleEmailChange}
             name="from_name"
+            isDisabled={messageSent}
+            ref={emailRef}
             id='1'
             className="max-w-xs mx-auto"
             />
@@ -48,10 +79,14 @@ const Contact = () => {
             variant='bordered'
             label="Message"
             name="message"
-            id='3'
+            value={messageValue}
+            onChange={handleMessageChange}
+            isDisabled={messageSent}
+            ref={messageRef}
+            id='2'
             className="max-w-xs mx-auto"
             />
-            <input type="submit" value="Send" className='active:bg-blue-900 px-6 py-2 bg-[#006FEE] mx-auto rounded-lg font-bold text-white cursor-pointer'/>
+            <Button disabled={messageSent} onClick={handleButton} ref={buttonRef} id type="submit" value="Send" className='m-auto font-bold text-white' isLoading={messageSent} color='primary'>{messageSent ? "Sending Message" : "Send Message"}</Button>
             </form>
             <p className='semibold mx-auto text-gray-400 pt-8 md:pt-24 flex flex-col justify-center align-middle text-center md:text-md text-xs'>Or just send me an email at <a href="mailto: mateusz-orlowski@outlook.com" className='text-[#006FEE] font-bold text-center mx-auto md:text-md text-xs'>mateusz-orlowski@outlook.com</a></p>
         </motion.div>
