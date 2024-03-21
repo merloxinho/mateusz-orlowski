e686f7fab796c315b1caf567b0196382a2219e37
merlox <150959690+merloxinho@users.noreply.github.com>
2024-03-21 02:12:34 +0100

    updated quickshare thumbnail in projects section

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -11,7 +11,7 @@ const Projects = () => {
         <div className='mx-auto flex my-auto flex-col w-full md:w-1/2 select-none text-balance md:text-pretty gap-4 text-center'>
             <motion.h1 viewport={{once:true}} initial={{scale:0}} whileInView={{scale:1}} transition={{delay:0.5}} className='font-bold text-4xl md:text-6xl'>My Projects ✨</motion.h1>
         </div>
-        <motion.div viewport={{once:true}} initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay:0.5}} className='mx-auto flex flex-col md:flex-row justify-center gap-24 w-5/6 select-none z-10 '>
+        <motion.div viewport={{once:true}} initial={{opacity:0}} whileInView={{opacity:1}} transition={{delay:0.5}} className='mx-auto flex flex-col-reverse md:flex-row-reverse justify-center gap-24 w-5/6 select-none z-10 '>
             <Card className="py-4">
                 <CardHeader className="pb-0 pt-2 px-4 flex-col items-start">
                     <small className="text-default-500">React, Tailwind</small>
@@ -36,7 +36,7 @@ const Projects = () => {
                     <Image
                     alt="Card background"
                     className="object-cover rounded-xl drop-shadow-lg"
-                    src="https://imgur.com/IYPKNbC.png"
+                    src="https://imgur.com/DNVc394.png"
                     width={300}
                     />
                     <a href="https://quickshare-roan.vercel.app" className='md:text-md text-sm pt-4 m-auto font-semibold text-[#006FEE]'>View on Github <i className="fa-brands fa-github" /></a>
